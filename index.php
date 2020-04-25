98678a716c79ae05513990b9b2353621e325576c
joyluiz63 <joyluiz63@gmail.com>
2020-04-25 07:40:44 -0300

    Tentando resolver pendencias exibidas no VsCode

--- index.php
+++ index.php
@@ -8,6 +8,20 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
  */
 
- $root = new Usuario();
- $root -> loadById(3);
- echo $root;
+ //Retorna um usuario de acordo com o id informdo
+ //$root = new Usuario();
+ //$root -> loadById(3);
+ //echo $root;
+
+ //Retorna toda a lista de usuarios cadastrados
+ //$lista = Usuario::getList(); //Como o metodo getList é static, não necessita instanciar o objeto
+ //echo json_encode($lista);
+
+ //Carrega uma lista de usuarios buscando pelo login
+ //$search = Usuario::search("jo");
+ //echo json_encode($search);
+
+ //Carrega um usuario autenticado
+ $usuario = new Usuario;
+ $usuario -> login("joao", "123456");
+ echo $usuario;
